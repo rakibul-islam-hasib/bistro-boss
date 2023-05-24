ba48547d73cb6f687fa21425f5e74ddb3f81e10b
Hasib Hossain <boraborhasib@gmail.com>
2023-05-24 17:02:17 +0600

    Menu page updated

--- Client/src/components/pages/menu/Menu.jsx
+++ Client/src/components/pages/menu/Menu.jsx
@@ -3,12 +3,18 @@ import PageCover from '../../shared/PageCover';
 import menuBg from '../../../assets/menu/banner3.jpg';
 import { useMenu } from '../../../hooks/useMenu';
 import desertBg from '../../../assets/menu/dessert-bg.jpeg'
+import saladBg from '../../../assets/menu/salad-bg.jpg';
+import soupBg from '../../../assets/menu/soup-bg.jpg';
+import pizzaBg from '../../../assets/menu/pizza-bg.jpg';
 import MenuCategory from './MenuCategory';
 import SectionCover from '../../shared/SectionCover';
 const Menu = () => {
     const [menu] = useMenu();
     const offered = menu.filter(item => item.category === 'offered');
     const dessert = menu.filter(item => item.category === 'dessert').slice(0, 4);
+    const salad = menu.filter(item => item.category === 'salad').slice(0, 4);
+    const soup = menu.filter(item => item.category === 'soup').slice(0, 4);
+    const pizza = menu.filter(item => item.category === 'pizza').slice(0, 4);
     return (
         <div>
             <PageCover img={menuBg} />
@@ -16,6 +22,16 @@ const Menu = () => {
             {/* <div className="mt-24"></div> */}
             <SectionCover  title='dessert' subTitle={'Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.'} img={desertBg} />
             <MenuCategory  item={dessert} />
+            {/* Pizza  */}
+            <SectionCover  title='pizza' subTitle={'Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.'} img={pizzaBg} />
+            <MenuCategory  item={pizza} />
+            {/* SOUP  */}
+            <SectionCover  title='soup' subTitle={'Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.'} img={soupBg} />
+            <MenuCategory  item={soup} />
+            {/* SOUP  */}
+            <SectionCover  title='salad' 
+            subTitle={'Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.'} img={saladBg} />
+            <MenuCategory  item={salad} />
         </div>
     );
 };
--- Client/src/components/pages/menu/MenuCategory.jsx
+++ Client/src/components/pages/menu/MenuCategory.jsx
@@ -2,16 +2,19 @@ import React from 'react';
 import SectionTitle from '../../shared/SectionTitle';
 import MenuItem from '../../MenuItem/MenuItem';
 
-const MenuCategory = ({ item = [], sTitle, sBody }) => {
+const MenuCategory = ({ item = [], sTitle, sBody, btn = 'ORDER YOUR FAVOURITE FOOD' }) => {
     return (
         <>
             <div className="my-16">
-              {sTitle && <SectionTitle title={sTitle} body={sBody} />}
+                {sTitle && <SectionTitle title={sTitle} body={sBody} />}
             </div>
             <div className="grid my-8 w-[80%] mx-auto gap-10 md:grid-cols-2">
                 {
                     item.map(item => <MenuItem key={item._id} item={item} />)
                 }
+            </div>
+            <div className="text-center mt-5">
+                <button className='px-10 py-3 uppercase text-black hover:bg-black hover:text-white  duration-500 border-b-4 border-b-black rounded-xl'>{btn}</button>
             </div>
         </>
     );
