c9e6ca887667f432486d8cb3d44f6bbae33555da
Hasib Hossain <boraborhasib@gmail.com>
2023-05-25 12:43:36 +0600

    Register page updated

--- Client/src/components/pages/user/Register.jsx
+++ Client/src/components/pages/user/Register.jsx
@@ -1,9 +1,52 @@
 import React from 'react';
 import bgImg from '../../../assets/reservation/wood-grain-pattern-gray1x.png'
+import authImg from '../../../assets/others/authentication2.png'
 const Register = () => {
+    const handelFormSubmit = (e) => {
+        e.preventDefault()
+        const fromData = new FormData(e.target)
+        const data = Object.fromEntries(fromData)
+        console.log("🚀 ~ file: Register.jsx:9 ~ handelFormSubmit ~ data:", data)
+
+    }
     return (
-        <div className='' style={{backgroundImage : `url(${bgImg})`}}>
-            <h1>Register</h1>
+        <div className='min-h-screen lg:px-40 py-20 ' style={{ backgroundImage: `url(${bgImg})` }}>
+            <div className="shadow-lg py-11 shadow-indigo-500 grid grid-cols-1 lg:grid-cols-2" style={{ backgroundImage: `url(${bgImg})` }}>
+                <div className="">
+                    <img src={authImg} alt="" />
+                </div>
+                <div className="w-1/2 mx-auto">
+                    <h1 className='text-3xl text-center font-bold'>Register</h1>
+                    <form onSubmit={handelFormSubmit} action="">
+                        <div className="flex flex-col items-center">
+                            <div className="">
+                                <h1 className='ml-2'>Name</h1>
+                                <input
+                                    className='border-2 border-gray-400 outline-none w-full rounded-md px-2 py-1 mt-2'
+                                    type="text" name='name' placeholder='Name' />
+                            </div>
+                            <div className="my-5">
+                                <h1 className='ml-2'>Email</h1>
+                                <input
+                                    className='border-2 border-gray-400 outline-none w-full rounded-md px-2 py-1 mt-2'
+                                    type="text"
+                                    name='email'
+                                    placeholder='Email' />
+                            </div>
+                            <div className="">
+                                <h1 className='ml-2'>Password</h1>
+                                <input
+                                    className='border-2 border-gray-400 outline-none w-full rounded-md px-2 py-1 mt-2'
+                                    type="password"
+                                    name='password'
+                                    placeholder='Name' />
+                            </div>
+                            <p className='my-2 text-[#D1A054]'>Already registered? Go to log in</p>
+                            <button type='submit' className='w-full py-3 my-4 rounded-xl font-bold bg-[#D1A054]'>Register</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
         </div>
     );
 };
---
+++ Client/src/config/firebase/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDK6MOsHWf9BdV0B5WryML1WBSF325wO3g",
+  authDomain: "bistro-boss-1.firebaseapp.com",
+  projectId: "bistro-boss-1",
+  storageBucket: "bistro-boss-1.appspot.com",
+  messagingSenderId: "546647639359",
+  appId: "1:546647639359:web:1fec1cd29d854b2122ff8a"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
