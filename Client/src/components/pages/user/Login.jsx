92d3a33ccfb8772b6fb9a5765cb9ca2487c6974d
Hasib Hossain <boraborhasib@gmail.com>
2023-05-27 20:19:11 +0600

    Register page error  Updated

--- Client/src/components/pages/user/Login.jsx
+++ Client/src/components/pages/user/Login.jsx
@@ -3,6 +3,7 @@ import bgImg from '../../../assets/reservation/wood-grain-pattern-gray1x.png'
 import authImg from '../../../assets/others/authentication2.png'
 import { loadCaptchaEnginge, LoadCanvasTemplate, validateCaptcha } from 'react-simple-captcha';
 import { AuthContext } from '../../../providers/AuthProvider';
+import { Link } from 'react-router-dom';
 const Login = () => {
     const { login } = useContext(AuthContext);
     const handelFormSubmit = e => {
@@ -36,7 +37,7 @@ const Login = () => {
                     <img src={authImg} alt="" />
                 </div>
                 <div className="w-1/2 mx-auto">
-                    <h1 className='text-3xl text-center font-bold'>Register</h1>
+                    <h1 className='text-3xl text-center font-bold'>Login</h1>
                     <form onSubmit={handelFormSubmit} action="">
                         <div className="flex flex-col items-center">
                             <div className="my-5">
@@ -69,7 +70,7 @@ const Login = () => {
                                     name='captcha'
                                     placeholder='Captcha' />
                             </div>
-                            <p className='my-2 text-[#D1A054]'>Already registered? Go to log in</p>
+                            <p className='my-2 text-[#D1A054]'>New to Bistro ?  <Link to={'/register'} className='text-orange-300'>Register</Link></p>
                             <button type='submit' className='w-full py-3 my-4 rounded-xl font-bold bg-[#D1A054]'>Register</button>
                         </div>
                     </form>
--- Client/src/components/pages/user/Register.jsx
+++ Client/src/components/pages/user/Register.jsx
@@ -1,67 +1,103 @@
 import React, { useContext } from 'react';
-import bgImg from '../../../assets/reservation/wood-grain-pattern-gray1x.png'
-import authImg from '../../../assets/others/authentication2.png'
+import { useForm } from 'react-hook-form';
+// import { trigger } from 'react-hook-form'; // Add this import
+import bgImg from '../../../assets/reservation/wood-grain-pattern-gray1x.png';
+import authImg from '../../../assets/others/authentication2.png';
 import { AuthContext } from '../../../providers/AuthProvider';
+import { Link } from 'react-router-dom';
+
 const Register = () => {
-    const { signUp , user } = useContext(AuthContext); 
+  const { signUp, user } = useContext(AuthContext);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    trigger
+  } = useForm();
 
-    // if (user) {
-    //     return <Navigate to='/' />
-    // }
+  const onSubmit = (data) => {
+    signUp(data.email, data.password)
+      .then((result) => {})
+      .catch((err) => {
+        console.log(err.code);
+      });
+  };
 
-    const handelFormSubmit = (e) => {
-        e.preventDefault()
-        const fromData = new FormData(e.target)
-        const data = Object.fromEntries(fromData)
-        signUp(data.email, data.password)
-        .then(result => {})
-        .catch(err => { 
-            console.log(err.code)
-        })
-    }
-    console.log(user , 'user from register')
-    return (
-        <div className='min-h-screen lg:px-40 py-20 ' style={{ backgroundImage: `url(${bgImg})` }}>
-            <div className="shadow-lg py-11 shadow-indigo-500 grid grid-cols-1 lg:grid-cols-2" style={{ backgroundImage: `url(${bgImg})` }}>
-                <div className="">
-                    <img src={authImg} alt="" />
-                </div>
-                <div className="w-1/2 mx-auto">
-                    <h1 className='text-3xl text-center font-bold'>Register</h1>
-                    <form onSubmit={handelFormSubmit} action="">
-                        <div className="flex flex-col items-center">
-                            <div className="">
-                                <h1 className='ml-2'>Name</h1>
-                                <input
-                                    className='border-2 border-gray-400 outline-none w-full rounded-md px-2 py-1 mt-2'
-                                    type="text" name='name' placeholder='Name' />
-                            </div>
-                            <div className="my-5">
-                                <h1 className='ml-2'>Email</h1>
-                                <input
-                                    className='border-2 border-gray-400 outline-none w-full rounded-md px-2 py-1 mt-2'
-                                    type="email"
-                                    required
-                                    name='email'
-                                    placeholder='Email' />
-                            </div>
-                            <div className="">
-                                <h1 className='ml-2'>Password</h1>
-                                <input
-                                    className='border-2 border-gray-400 outline-none w-full rounded-md px-2 py-1 mt-2'
-                                    type="password"
-                                    required
-                                    name='password'
-                                    placeholder='Name' />
-                            </div>
-                            <p className='my-2 text-[#D1A054]'>Already registered? Go to log in</p>
-                            <button type='submit' className='w-full py-3 my-4 rounded-xl font-bold bg-[#D1A054]'>Register</button>
-                        </div>
-                    </form>
-                </div>
-            </div>
+  return (
+    <div className='min-h-screen lg:px-40 py-20' style={{ backgroundImage: `url(${bgImg})` }}>
+      <div
+        className='shadow-lg py-11 shadow-indigo-500 grid grid-cols-1 lg:grid-cols-2'
+        style={{ backgroundImage: `url(${bgImg})` }}
+      >
+        <div className=''>
+          <img src={authImg} alt='' />
         </div>
-    );
+        <div className='w-1/2 mx-auto'>
+          <h1 className='text-3xl text-center font-bold'>Register</h1>
+          <form onSubmit={handleSubmit(onSubmit)} action=''>
+            <div className='flex flex-col items-center'>
+              <div className=''>
+                <h1 className='ml-2'>Name</h1>
+                <input
+                  className='border-2 border-gray-400 outline-none w-full rounded-md px-2 py-1 mt-2'
+                  type='text'
+                  name='name'
+                  {...register('name', { required: true })}
+                  placeholder='Name'
+                />
+                {errors.name && (
+                  <span className='text-xs text-red-500 ml-2'>This field is required</span>
+                )}
+              </div>
+              <div className='my-2'>
+                <h1 className='ml-2'>Email</h1>
+                <input
+                  className='border-2 border-gray-400 outline-none w-full rounded-md px-2 py-1 mt-2'
+                  type='email'
+                  {...register('email', { required: true })}
+                  name='email'
+                  placeholder='Email'
+                />
+                {errors.email && (
+                  <span className='text-xs text-red-500 ml-2'>Email is required</span>
+                )}
+              </div>
+              <div className=''>
+                <h1 className='ml-2'>Password</h1>
+                <input
+                  className='border-2 border-gray-400 outline-none w-full rounded-md px-2 py-1 mt-2'
+                  type='password'
+                  {...register('password', { required: true, minLength: 6 })}
+                  name='password'
+                  placeholder='Password'
+                  onChange={() => {
+                    trigger('password');
+                  }}
+                />
+                {errors.password && errors.password.type === 'required' && (
+                  <span className='text-xs text-red-500 ml-2'>Password is required</span>
+                )}
+                {errors.password && errors.password.type === 'minLength' && (
+                  <span className='text-xs text-red-500 ml-2'>
+                    Password must be at least 6 characters long
+                  </span>
+                )}
+              </div>
+              <p className='my-2 text-[#D1A054]'>
+                Already registered? Go to <Link to={'/login'}>log in</Link>
+              </p>
+              <button
+                type='submit'
+                className='w-full py-3 my-4 rounded-xl font-bold bg-[#D1A054]'
+              >
+                Register
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Register;
