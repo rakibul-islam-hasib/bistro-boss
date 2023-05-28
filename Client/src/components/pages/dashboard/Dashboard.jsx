02555017128fc2bea58f8c6bf229e439a41e7be4
Hasib Hossain <boraborhasib@gmail.com>
2023-05-28 22:49:26 +0600

    Dash board updated

--- Client/src/components/pages/dashboard/Dashboard.jsx
+++ Client/src/components/pages/dashboard/Dashboard.jsx
@@ -1,9 +1,50 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import { AiFillHome, AiOutlineShoppingCart} from 'react-icons/ai';
+import { BsFillCalendarCheckFill } from 'react-icons/bs';
+import { ImSpoonKnife } from 'react-icons/im';
+import { NavLink } from 'react-router-dom';
 const Dashboard = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleSidebar = () => {
+        setIsOpen(!isOpen);
+    };
     return (
-        <div>
-            <h1>Dashboard</h1>
+        <div className="grid grid-cols-10">
+            <div className="col-span-2 font-Cinzel px-5 py-5 bg-[#D1A054] min-h-screen">
+                <div className="text-center mt-4">
+                    <h1 className='uppercase text-2xl font-Cinzel font-bold'>Bistro BOSS</h1>
+                    <p className='tracking-[0.6rem] font-Cinzel'>Restaurant</p>
+                </div>
+                <div className="mt-6 px-5">
+                    <ul>
+                        <li>
+                            <NavLink>
+                                <span className='inline-flex w-full items-center uppercase text-base gap-4'><AiFillHome className='text-2xl' /> Home</span>
+                            </NavLink>
+                        </li>
+                        <li className='mt-4'>
+                            <NavLink>
+                                <span className='inline-flex w-full uppercase items-center text-base gap-4'>
+                                    <BsFillCalendarCheckFill className='text-2xl' /> Reservation</span>
+                            </NavLink>
+                        </li>
+                        <li className='mt-4'>
+                            <NavLink>
+                                <span className='inline-flex w-full whitespace-nowrap uppercase items-center text-base gap-4'>
+                                    <ImSpoonKnife className='text-2xl' /> Payment history</span>
+                            </NavLink>
+                        </li>
+                        <li className='mt-4'>
+                            <NavLink>
+                                <span className='inline-flex w-full whitespace-nowrap uppercase items-center text-base gap-4'>
+                                    <AiOutlineShoppingCart className='text-2xl' /> My Cart </span>
+                            </NavLink>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div className="col-span-4"></div>
         </div>
     );
 };
