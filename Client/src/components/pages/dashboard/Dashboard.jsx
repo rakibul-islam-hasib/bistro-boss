39ee57d23a2a71a69e13fe428e9d3248472e7642
Hasib Hossain <boraborhasib@gmail.com>
2023-05-28 22:59:08 +0600

    Dashboard updated

--- Client/src/components/pages/dashboard/Dashboard.jsx
+++ Client/src/components/pages/dashboard/Dashboard.jsx
@@ -1,14 +1,45 @@
 import React, { useState } from 'react';
-import { AiFillHome, AiOutlineShoppingCart} from 'react-icons/ai';
+import { AiFillHome, AiOutlineShoppingCart } from 'react-icons/ai';
 import { BsFillCalendarCheckFill } from 'react-icons/bs';
+import { FaSwatchbook } from 'react-icons/fa';
 import { ImSpoonKnife } from 'react-icons/im';
-import { NavLink } from 'react-router-dom';
+import { TbStarsFilled } from 'react-icons/tb';
+import { NavLink, Outlet } from 'react-router-dom';
+
 const Dashboard = () => {
     const [isOpen, setIsOpen] = useState(false);
 
     const toggleSidebar = () => {
         setIsOpen(!isOpen);
     };
+
+    const menuItems = [
+        {
+            icon: <AiFillHome className='text-2xl' />,
+            text: 'Home',
+        },
+        {
+            icon: <BsFillCalendarCheckFill className='text-2xl' />,
+            text: 'Reservation',
+        },
+        {
+            icon: <ImSpoonKnife className='text-2xl' />,
+            text: 'Payment history',
+        },
+        {
+            icon: <AiOutlineShoppingCart className='text-2xl' />,
+            text: 'My Cart',
+        },
+        {
+            icon: <TbStarsFilled className='text-2xl' />,
+            text: 'My Reviews',
+        },
+        {
+            icon: <FaSwatchbook className='text-2xl' />,
+            text: 'My Bookings',
+        },
+    ];
+
     return (
         <div className="grid grid-cols-10">
             <div className="col-span-2 font-Cinzel px-5 py-5 bg-[#D1A054] min-h-screen">
@@ -18,33 +49,21 @@ const Dashboard = () => {
                 </div>
                 <div className="mt-6 px-5">
                     <ul>
-                        <li>
-                            <NavLink>
-                                <span className='inline-flex w-full items-center uppercase text-base gap-4'><AiFillHome className='text-2xl' /> Home</span>
-                            </NavLink>
-                        </li>
-                        <li className='mt-4'>
-                            <NavLink>
-                                <span className='inline-flex w-full uppercase items-center text-base gap-4'>
-                                    <BsFillCalendarCheckFill className='text-2xl' /> Reservation</span>
-                            </NavLink>
-                        </li>
-                        <li className='mt-4'>
-                            <NavLink>
-                                <span className='inline-flex w-full whitespace-nowrap uppercase items-center text-base gap-4'>
-                                    <ImSpoonKnife className='text-2xl' /> Payment history</span>
-                            </NavLink>
-                        </li>
-                        <li className='mt-4'>
-                            <NavLink>
-                                <span className='inline-flex w-full whitespace-nowrap uppercase items-center text-base gap-4'>
-                                    <AiOutlineShoppingCart className='text-2xl' /> My Cart </span>
-                            </NavLink>
-                        </li>
+                        {menuItems.map((item, index) => (
+                            <li key={index} className="mt-4">
+                                <NavLink>
+                                    <span className='inline-flex w-full whitespace-nowrap uppercase items-center text-base gap-4'>
+                                        {item.icon} {item.text}
+                                    </span>
+                                </NavLink>
+                            </li>
+                        ))}
                     </ul>
                 </div>
             </div>
-            <div className="col-span-4"></div>
+            <div className="col-span-4">
+                <Outlet />
+            </div>
         </div>
     );
 };
